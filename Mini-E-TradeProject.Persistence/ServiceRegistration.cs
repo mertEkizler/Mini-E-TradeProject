27f7741452f4270a38d2154663654c0d8eed2d7b
mertekizler1 <mert.ekizler@valprovia.com>
2022-05-05 15:13:04 +0300

    code refactoring

--- Mini-E-TradeProject.Persistence/ServiceRegistration.cs
+++ Mini-E-TradeProject.Persistence/ServiceRegistration.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Mini_E_TradeProject.Persistence.Contexts;
 using Microsoft.EntityFrameworkCore;
+using Mini_E_TradeProject.Application.Repositories;
+using Mini_E_TradeProject.Persistence.Repositories;
 
 namespace Mini_E_TradeProject.Persistence
 {
@@ -12,6 +14,15 @@ namespace Mini_E_TradeProject.Persistence
                 (
                     options => options.UseSqlServer(Configuration.ConnectionString)
                 );
+
+            services.AddScoped<ICustomerReadRepository, CustomerReadRepository>();
+            services.AddScoped<ICustomerWriteRepository, CustomerWriteRepository>();
+
+            services.AddScoped<IOrderWriteRepository, OrderWriteRepository>();
+            services.AddScoped<IOrderReadRepository, OrderReadRepository>();
+
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
         }
     }
 }
